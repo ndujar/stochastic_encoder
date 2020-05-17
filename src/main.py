aa8d6665daf44d531df68b554a710ecd5d1e3fa9
Rabindranath <rabindranath.andujar@gmail.com>
2020-05-17 03:14:13 +0200

    Fixes issue with fraem dimensions

--- src/main.py
+++ src/main.py
@@ -24,71 +24,115 @@ def main():
     cap = cv2.VideoCapture(ARGS.source)
 
     start_time = time.time()
-    # Collect the pixels of the first frame
-    _, prev_frame = cap.read()
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    pixel_trajectories = prev_frame
-    pixel_trajectories_blue = []
-    pixel_trajectories_green = []
-    pixel_trajectories_red = []
-    test_pixel_coordinates = (435, 1000)
+    max_frames = 4
+    print(height, width, max_frames)
+    scale_percent = 50 # percent of original size
+    width = int(width * scale_percent / 100)
+    height = int(height * scale_percent / 100)
+    dim = (width, height)
+    # resize image
+    pixel_trajectories_blue = np.zeros((height, width), dtype=np.uint8)
+    pixel_trajectories_green = np.zeros((height, width), dtype=np.uint8)
+    pixel_trajectories_red = np.zeros((height, width), dtype=np.uint8)
+    test_pixel_coordinates = (int(height * np.random.random()), int(width * np.random.random()))
+    print('Testing in', test_pixel_coordinates)
     pixel_trajectory = []
     # Read until video is completed
-    frame_count = 1
-    while cap.isOpened():
+    frame_count = 0
+    while True:
     # Capture frame-by-frame
         ret, frame = cap.read()
-        if ret and frame_count < 15:
+        if ret:
             frame_count += 1
-            compressed_frame = compress_image(frame, 98)
-            pixel_trajectories_blue.append(compressed_frame[:, :, 0])
-            pixel_trajectories_green.append(compressed_frame[:, :, 1])
-            pixel_trajectories_red.append(compressed_frame[:, :, 2])
-            pixel_trajectory.append(compressed_frame[test_pixel_coordinates][0])
+            print(frame_count)
+            if frame_count % 3 == 0:
+
+                frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA) 
+
+                compressed_frame = compress_image(frame, 95)
+                pixel_trajectories_blue = np.dstack((pixel_trajectories_blue, compressed_frame[:, :, 0]))
+                pixel_trajectories_green = np.dstack((pixel_trajectories_green, compressed_frame[:, :, 1]))
+                pixel_trajectories_red = np.dstack((pixel_trajectories_red, compressed_frame[:, :, 2]))
+
+                pixel_trajectory.append(compressed_frame[test_pixel_coordinates[0],
+                                                        test_pixel_coordinates[1],
+                                                        0])
+                print('Pixel trajectory:', pixel_trajectory)
+                print('Stored frames:', pixel_trajectories_blue.shape)
+                if pixel_trajectories_blue.shape[2] == max_frames:
+                    break
         # Break the loop
         else:
             break
 
     # When everything done, release the video capture object
     cap.release()
+
     print(f'Elapsed time: {time.time() - start_time}, Frames: {frame_count}')
-
-    # diff_blue = np.diff(np.asarray(pixel_trajectories_blue), axis=0)
-    # diff_green = np.diff(np.asarray(pixel_trajectories_green), axis=0)
-    # diff_red = np.diff(np.asarray(pixel_trajectories_red), axis=0)
-
-    encoder = np.zeros((frame_count-1), dtype=np.float128) + 10
-    exponents = np.arange(frame_count-1, dtype=np.uint16) * 3
-    encoder = 1 / np.power(encoder, exponents)
-    np.set_printoptions(precision=frame_count*2, suppress=True)
-    print('Encoder:', encoder)
-    shape = np.asarray(pixel_trajectories_blue).shape
-    pixel_trajectories_blue = np.asarray(pixel_trajectories_blue).reshape((shape[2], shape[1], shape[0]))
-    encoded_blue = np.dot(pixel_trajectories_blue, encoder)
-    print('Pixel trajectories:', pixel_trajectories_blue[test_pixel_coordinates, :])
     print('Pixel trajectory:', pixel_trajectory)
-    print('Encoded trajectory: {0:.100f}'.format(encoded_blue[test_pixel_coordinates[1], test_pixel_coordinates[0]]))
-    np.savez_compressed('blue.npz', encoded_blue)
+
+    pixel_trajectories_blue = np.asarray(pixel_trajectories_blue)
+    diff_green = np.asarray(pixel_trajectories_green)
+    diff_red = np.asarray(pixel_trajectories_red)
+
+    np.set_printoptions(precision=max_frames*2,
+                        suppress=True,
+                        threshold=sys.maxsize)
+
+    encoder = np.zeros((max_frames), dtype=np.float64) + 10
+    exponents = np.asarray(range(max_frames)) * 3
+
+    encoder = 1 * np.power(encoder, exponents)
+
+    print('Exponents:', exponents)
+    print('Encoder.', encoder)
+    encoded_blue = np.matmul(pixel_trajectories_blue, encoder, dtype=np.float64) + 10 ** 12
+    print('Encoded B channel shape:', encoded_blue.shape)
+    print('Encoded B channel size in system:', sys.getsizeof(encoded_blue))
+    print('Pixel trajectories:', pixel_trajectories_blue[test_pixel_coordinates[0], test_pixel_coordinates[1], :], pixel_trajectories_blue.shape)
+    
+    print('Encoded trajectory: {:f}'.format(encoded_blue[test_pixel_coordinates[0], test_pixel_coordinates[1]]))
+    encoded_path = 'blue.npz'
+    np.savez_compressed(encoded_path, encoded_blue)
+    b = os.path.getsize(encoded_path)
+    print('Compressed encoded B channel size in system:', b)
     print('Elapsed time:', time.time() - start_time)
- 
-    # data_plot = []
-    # for column in np.arange(diff_blue.shape[1]):
-    #     for row in np.arange(diff_blue.shape[2]):
-    #         data_plot.append(go.Scatter(x=np.arange(diff_blue.shape[0]), y=diff_blue[:, column, row]))
+    print('FPS:', frame_count / (time.time() - start_time))
+    data_plot = []
+
+    with np.load(encoded_path) as data:
+        encoded_blue = data['arr_0'] - 10 ** 12
+    decoder = -np.sort(-encoder)
+    for step in np.nditer(decoder):
+        print(decoder, step)
+        print(encoded_blue[test_pixel_coordinates])
+        frame = encoded_blue // step
+        
+        print(frame[test_pixel_coordinates])
+        # cv2.imshow('decoded', frame)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
+    
+    data.close()
+    # for column in np.arange(pixel_trajectories_blue.shape[1]):
+    #     for row in np.arange(pixel_trajectories_blue.shape[2]):
+    #         data_plot.append(go.Scatter(x=np.arange(pixel_trajectories_blue.shape[0]), y=pixel_trajectories_blue[:, column, row]))
     # # data_plot.append(go.Scatter(x=np.arange(diff_green.shape[0]), y=diff_green[:, 100, 400]))
     # # data_plot.append(go.Scatter(x=np.arange(diff_red.shape[0]), y=diff_red[:, 100, 400]))
     # fig = go.Figure(data=data_plot)
     # st.write(fig)
     
-    # data_plot = []
-    # data_plot.append(go.Scatter(x=np.arange(frame_count), y=np.asarray(pixel_trajectories_blue)[:, 100, 400]))
+    data_plot = []
+    data_plot.append(go.Scatter(x=np.arange(frame_count), y=pixel_trajectories_blue[test_pixel_coordinates[0], test_pixel_coordinates[1], :]))
     # data_plot.append(go.Scatter(x=np.arange(frame_count), y=np.asarray(pixel_trajectories_green)[:, 100, 400]))
     # data_plot.append(go.Scatter(x=np.arange(frame_count), y=np.asarray(pixel_trajectories_red)[:, 100, 400]))
 
-    # fig = go.Figure(data=data_plot)
-    # st.write(fig)
+    fig = go.Figure(data=data_plot)
+    st.write(fig)
     
-    cv2.destroyAllWindows()
 if __name__ == '__main__':
 
     main()
